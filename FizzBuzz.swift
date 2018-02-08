9a600171486cbf62a7899c633f799f6092f90466
Ashika Balamurugan <ashikabala01@gmail.com>
2018-02-07 19:12:46 -0800

    FizzBuzz Problem

---
+++ FizzBuzz.swift
@@ -0,0 +1,19 @@
+//
+//  FizzBuzz.swift
+//
+//Problem: https://leetcode.com/problems/fizz-buzz/description/
+
+class Solution {
+    func fizzBuzz(_ n: Int) -> [String] {
+        var res = [String]()
+        if n == 0 {return res}
+        for i in 1...n{
+            if (i%3 == 0) && (i%5 == 0) {res.append("FizzBuzz")}
+            else if i%3 == 0 {res.append("Fizz")}
+            else if i%5 == 0 {res.append("Buzz")}
+            else {res.append("\(i)")}
+        }
+        
+        return res
+    }
+}
