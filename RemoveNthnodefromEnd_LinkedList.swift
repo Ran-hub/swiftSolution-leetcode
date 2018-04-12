404814fe5852f76ab52617b201fbca190057a753
Ashika Balamurugan <ashikabala01@gmail.com>
2018-04-12 12:08:13 -0700

    Remove Kth node from end - LinkedList

--- RemoveNthnodefromEnd_LinkedList.swift
+++ RemoveNthnodefromEnd_LinkedList.swift
@@ -1,8 +1,26 @@
 //
 //  RemoveNthnodefromEnd_LinkedList.swift
-//  
-//
-//  Created by ashika shanthi on 4/12/18.
-//
-
-import Foundation
+class Solution {
+    func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        let dummy = ListNode(0)
+        dummy.next = head
+        var start: ListNode? = dummy
+        var end: ListNode? = dummy
+        var counter = 0
+        
+        while end != nil {
+            if counter <= n {
+                end = end?.next
+                counter += 1
+            }
+            else{
+                start = start?.next
+                end = end?.next
+            }
+        }
+        //delete node
+        start?.next = start?.next?.next
+        
+        return dummy.next
+    }
+}
